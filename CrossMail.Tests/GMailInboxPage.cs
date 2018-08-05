0b2cc5cd1da23571c3f4f7591cdf7db67fd292c4
Warley <warleylopes@outlook.com>
2018-08-05 20:17:24 -0300

    Add files via upload

---
+++ CrossMail.Tests/GMailInboxPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace CrossMail.Tests
+{
+    public class GMailInboxPage
+    {
+        private IWebDriver driver;
+        public GMailInboxPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//*[@role='button' and (.)='COMPOSE']")]
+        private IWebElement composeElement;
+
+        [FindsBy(How = How.Name, Using = "to")]
+        private IWebElement destinationInput;
+
+        [FindsBy(How = How.XPath, Using = "//*[@role='button' and text()='Send']")]
+        private IWebElement sendButtonElement;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=':a5']")]
+        private IWebElement bodyTextBoxElement;
+
+        public void SendMail(string destination)
+        {
+            //Needs improvement because of user locale while running the webdriver
+            //Needs more implementation since the scope of testing isn't complete
+            composeElement.Click();
+            destinationInput.Clear();
+            destinationInput.SendKeys(destination);
+            bodyTextBoxElement.SendKeys(destination);
+            sendButtonElement.Click();
+        }
+    }
+}
---
+++ CrossMail.Tests/GMailLoginPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace CrossMail.Tests
+{
+    public class GMailLoginPage
+    {
+        private IWebDriver driver;
+        public GMailLoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "identifierId")]
+        private IWebElement userInput;
+
+        [FindsBy(How = How.Id, Using = "identifierNext")]
+        private IWebElement userInputNextButton;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='password']//input")]
+        private IWebElement passInput;
+
+        [FindsBy(How = How.Id, Using = "passwordNext")]
+        private IWebElement passInputNextButton;
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://mail.google.com/");
+        }
+
+        public GMailInboxPage Login(string user, string password)
+        {
+            userInput.SendKeys(user);
+            userInputNextButton.Click();
+            
+            passInput.SendKeys(password);
+            passInputNextButton.Click();
+
+            return new GMailInboxPage(driver);
+        }
+    }
+}
