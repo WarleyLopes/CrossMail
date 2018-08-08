d3ae63031947f2fa230b365045029cd9c2d34625
Warley <warleylopes@outlook.com>
2018-08-07 23:22:09 -0300

    Add files via upload
    Continuation of the test implementation.

---
+++ GMailInboxPage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System.IO;
+
+namespace CrossMail.Tests
+{
+    public class GMailInboxPage
+    {
+        private IWebDriver driver;
+        public GMailInboxPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//*[@role='button' and (.)='COMPOSE']")]
+        private IWebElement composeElement;
+
+        [FindsBy(How = How.Name, Using = "to")]
+        private IWebElement destinationInput;
+
+        [FindsBy(How = How.XPath, Using = "//*[@role='button' and text()='Send']")]
+        private IWebElement sendButtonElement;
+
+        [FindsBy(How = How.XPath, Using = "//input[@name='subjectbox']")]
+        private IWebElement subjectBoxElement;
+
+        [FindsBy(How = How.XPath, Using = "//div[@aria-label='Message Body']")]
+        private IWebElement bodyTextBoxElement;
+
+        [FindsBy(How = How.XPath, Using = "//input[@name='Filedata']")]
+        private IWebElement attachFileElement;
+
+        public void SendMail(string destination, string subject, string bodyText, string attachment = "")
+        {
+            //Needs improvement because of user locale while running the webdriver
+            composeElement.Click();
+            destinationInput.Clear();
+            destinationInput.SendKeys(destination);
+            subjectBoxElement.SendKeys(subject);
+            bodyTextBoxElement.SendKeys(bodyText);
+            if (attachment != "") {
+                attachFileElement.SendKeys(Directory.GetCurrentDirectory() + "\\" + attachment);
+            }
+            sendButtonElement.Click();
+        }
+    }
+}
---
+++ GMailTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace CrossMail.Tests
+{
+    public class GMailTests: IDisposable
+    {
+        IWebDriver _browserDriver;
+        IConfiguration _config;
+        public GMailTests()
+        {
+            _browserDriver = new ChromeDriver("./");
+
+            _browserDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _browserDriver.Manage().Window.Maximize();
+
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .Build();
+        }
+
+        public void Dispose()
+        {
+            _browserDriver.Quit();
+        }
+
+        [Fact]
+        public void Should_Send_Email()
+        {
+            GMailLoginPage gMailLoginPage = new GMailLoginPage(_browserDriver);
+
+            gMailLoginPage.goToPage();
+
+            GMailInboxPage gMailInboxPage = gMailLoginPage.Login(_config["username"], _config["password"]);
+
+            gMailInboxPage.SendMail($"{_config["username"]}@gmail.com", "CrossMail Testing", "Just a body text for a e-mail test automation", "config.json");
+
+            //Needs improvement because of user locale while running the webdriver (compose button, 'to' field changes)
+            //Needs more implementation since the scope of testing isn't complete
+        }
+    }
+}
